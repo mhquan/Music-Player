5e9a78a1263ed374b251c0e631aaed04d04ddf8e
Quan <quan@Quans-Mac-mini.local>
2017-08-21 17:42:50 +0700

    Initial code

---
+++ Music Player/FirstViewController.swift
@@ -0,0 +1,74 @@
+//
+//  FirstViewController.swift
+//  Music Player
+//
+//  Created by QuanMH on 8/21/17.
+//  Copyright © 2017 Quan. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+var audioPlayer = AVAudioPlayer()
+var songs:[String] = ["1","2","3","4","5","6"]
+var thisSong = 0
+var audioStuffed = false
+
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
+    @IBOutlet weak var myTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        gettingSongNames()
+    }
+
+    func gettingSongNames() {
+        let folderURL = URL(fileURLWithPath:Bundle.main.resourcePath!)
+        do {
+            let songPath = try FileManager.default.contentsOfDirectory(at: folderURL, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
+            
+            //loop through the found urls
+            for song in songPath{
+                var mySong = song.absoluteString
+                if mySong.contains(".mp3") {
+                    let findString = mySong.components(separatedBy: "/")
+                    mySong = findString[findString.count-1]
+                    mySong = mySong.replacingOccurrences(of: "%20", with: " ")
+                    mySong = mySong.replacingOccurrences(of: ".mp3", with: "")
+                    songs.append(mySong)
+                }
+            }
+            myTableView.reloadData()
+        }
+        catch{
+            print ("ERROR")
+        }
+    }
+    
+    
+    //MARK: set up table view
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return songs.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = songs[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        do {
+            let audioPath = Bundle.main.path(forResource: songs[indexPath.row], ofType: ".mp3")
+            try audioPlayer = AVAudioPlayer(contentsOf: NSURL(fileURLWithPath: audioPath!) as URL)
+            audioPlayer.play()
+            thisSong = indexPath.row
+            audioStuffed = true
+        } catch{
+            print ("ERROR")
+        }
+    }
+
+}
